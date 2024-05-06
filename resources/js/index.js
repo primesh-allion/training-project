ddd0c7d4cc9895402e7298942fa3fe9b732f98be
Primeshheshan <primeshheshan@gmail.com>
2024-05-06 16:54:00 +0530

    feat: navbar complete

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,4 @@
-
+document.getElementById("toggleMenu").addEventListener("click", function () {
+  var menuItems = document.querySelector(".navbar .menu-items");
+  menuItems.classList.toggle("open");
+});
