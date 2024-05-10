313eda78db0dfbb5b6ff2d72b3ce32a807862be8
primesh-allion <primeshallion@gmail.com>
2024-05-10 13:59:01 +0530

    feat: typography

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,4 @@
-document.getElementById("toggleMenu").addEventListener("click", function () {
-  var menuItems = document.querySelector(".navbar .menu-items");
-  menuItems.classList.toggle("open");
-});
+// document.getElementById("toggleMenu").addEventListener("click", function () {
+//   var menuItems = document.querySelector(".navbar .menu-items");
+//   menuItems.classList.toggle("open");
+// });
