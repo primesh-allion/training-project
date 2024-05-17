00e3b0432f69f994a9aa574879f544520af19705
primesh-allion <primeshallion@gmail.com>
2024-05-17 16:51:26 +0530

    feat: complete ui

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,4 @@
-// document.getElementById("toggleMenu").addEventListener("click", function () {
-//   var menuItems = document.querySelector(".navbar .menu-items");
-//   menuItems.classList.toggle("open");
-// });
+function toggle() {
+  var menuItems = document.getElementById("toggleMenu");
+  menuItems.classList.toggle("hidden");
+}
